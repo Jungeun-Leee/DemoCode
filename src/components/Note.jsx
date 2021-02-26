5b8f0857428f621aaf6a32549cff83403d17b923
Shrestha <shresthas6@wit.edu>
2021-02-26 11:47:04 -0500

    initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is a drill</h1>
+      <p>checking if we can deploye our website</p>
+    </div>
+  );
+}
+
+export default Note;
